1225ec6c18ae8bc3ff7bd76d19fafad70febd8e3
davidrobertlowe <davidrobertlowe@gmail.com>
2022-03-26 00:52:13 -0700

    depature_location

--- src/rides/rides.controller.ts
+++ src/rides/rides.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('rides')
-export class RidesController {}
+export class RidesController {
+    @Get()
+    findAll(): string {
+    return 'This action returns all locations';
+  }
+
+}
