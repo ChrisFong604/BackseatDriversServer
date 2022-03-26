b099640707f16a9632ceea0d7839163b17f3b14c
ChrisFong604 <ccffoonngg@gmail.com>
2022-03-25 18:29:22 -0700

    added riders endpoint and swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ControllerController } from './controller/controller.controller';
+import { RidersController } from './riders/riders.controller';
+import { RidersModule } from './riders/riders.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [RidersModule],
+  controllers: [AppController, ControllerController, RidersController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('BackseatDriverAPI')
+    .setDescription('All API endpoints')
+    .setVersion('1.0')
+    .addTag('Rides')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: '*',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
+  await app.listen(5000);
 }
 bootstrap();
