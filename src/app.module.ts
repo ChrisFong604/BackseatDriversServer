376a7f8d077b9add018c51be927f2384e47176c5
ChrisFong604 <ccffoonngg@gmail.com>
2022-03-25 23:51:53 -0700

    added initial modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,14 @@ import { RidersController } from './riders/riders.controller';
 import { RidersModule } from './riders/riders.module';
 import { RidesController } from './rides/rides.controller';
 import { RidesModule } from './rides/rides.module';
+import { DriversController } from './drivers/drivers.controller';
+import { RequestsController } from './requests/requests.controller';
+import { RequestsModule } from './requests/requests.module';
+import { DriversModule } from './drivers/drivers.module';
 
 @Module({
-  imports: [RidersModule, RidesModule],
-  controllers: [AppController, ControllerController, RidersController, RidesController],
+  imports: [RidersModule, RidesModule, RequestsModule, DriversModule],
+  controllers: [AppController, ControllerController, RidersController, RidesController, DriversController, RequestsController],
   providers: [AppService],
 })
 export class AppModule {}
